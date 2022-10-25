168e8223d8a813e1becae4af033788e6ac6c7d02
saul-l <sauli.lehtinen@gmail.com>
2022-10-25 12:03:11 +0300

    added wav to git LFS

---
+++ Assets/Components/HelpersToolsExtensions/AudioSystem.cs
@@ -0,0 +1,20 @@
+#define WWISE
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class AudioSystem
+{
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+#if WWISE
+    public static void PlayAudioEvent(string AudioEvent)
+    {
+
+    }
+
+#endif
+
+}
